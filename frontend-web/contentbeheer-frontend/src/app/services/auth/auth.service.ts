d961edb07b7e179cca9ccba3d4ed1014c169ecbd
HarleenBhandalPXL <harleen.bhandal@student.pxl.be>
2025-01-02 21:05:58 +0100

    Added authservice to navbar

--- frontend-web/contentbeheer-frontend/src/app/services/auth/auth.service.ts
+++ frontend-web/contentbeheer-frontend/src/app/services/auth/auth.service.ts
@@ -6,8 +6,11 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   user: string | null = null;
   role: string | null = 'user';
+  roleKey = 'userRole';
 
-  constructor() { }
+  constructor() {
+    this.role = localStorage.getItem(this.roleKey) || 'user';
+  }
 
   setUser(user: string): void {
     this.user = user;
@@ -19,6 +22,7 @@ export class AuthService {
 
   setRole(role: string): void {
     this.role = role;
+    localStorage.setItem(this.roleKey, role);
   }
 
   getRole(): string | null {
@@ -28,9 +32,11 @@ export class AuthService {
   clearAuth(): void {
     this.user = null;
     this.role = null;
+    localStorage.removeItem(this.roleKey);
   }
 
   toggleRole(): void {
     this.role = this.role === 'editor' ? 'user' : 'editor';
+    localStorage.setItem(this.roleKey, this.role);
   }
 }
